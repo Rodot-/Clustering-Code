ab684c522d1eb1851199d32145bc99c95b98a7c1
Jack O'Brien <jto33@drexel.edu>
2015-05-05 10:07:27 -0400

    Added arg_ functions.

--- kmeans.py
+++ kmeans.py
@@ -18,6 +18,19 @@ def kmeans(data, means): #Kmeans clustering function
 		print "".join(( '\033[93m',"".join(('|' for i in xrange(K))))),'\033[92m','\033[0m' #prints bars indicating K for multi processing.
 	return array([data[clusters == i] for i in xrange(K)]), means
 
+def arg_kmeans(data, means): #returns the index array of the clusters
+
+	K = len(means)
+	old_means = array(means)*0
+	means = array(means)
+	data = array(data)
+	clusters = empty(len(data), dtype = '>i4') #An array of cluster indecies
+	while (old_means != means).any(): #Keep going until no change.
+		clusters[0:] = comparison(data, means)
+		old_means = means
+		means = array([mean(data[clusters == i], axis = 0) for i in xrange(K)])
+	return clusters, means
+
 def kmeans_struct(data, means): #For use with structured Array
 
 	data_view = data.view((data.dtype[0], len(data.dtype.names)))
@@ -31,6 +44,18 @@ def kmeans_struct(data, means): #For use with structured Array
 		means_view[::] = [mean(data_view[clusters == i], axis = 0) for i in xrange(K)]
 	return [data[clusters == i] for i in xrange(K)], means	
 
+def kmeans_struct(data, means): #For use with structured Array
+
+	data_view = data.view((data.dtype[0], len(data.dtype.names)))
+	means_view = means.view((means.dtype[0], len(means.dtype.names)))
+	old_means = empty(means_view.shape, dtype = means_view.dtype)
+	clusters = empty(data_view.shape[0], dtype = '>i4')
+	K = means_view.shape[0]
+	while (old_means != means_view).any():
+		clusters[::] = comparison(data_view, means_view)
+		old_means[::] = means_view[::]
+		means_view[::] = [mean(data_view[clusters == i], axis = 0) for i in xrange(K)]
+	return clusters, means	
 
 def kmeans_generator(data, *means): #Kmeans clustering function as a generator for each step.  Used to observe progress of clustering code.
 
