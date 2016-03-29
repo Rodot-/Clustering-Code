d6a8eec744d20074e2235fb9b14f27f390962cc9
rodot- <jto22@drexel.edu>
2016-03-29 15:02:41 -0400

    Added setup.py.  Now it should be a functioning module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+name = 'kmeans', 
+version = '0.2',
+description = 'Basic Library for K-Means Clustering',
+url = 'http://github.com/Rodot-/Clustering-Code',
+author = 'John (Jack) T O\'Brien',
+author_email = 'john.t.obrien@drexel.edu',
+license = 'GNU',
+packages = ['kmeans'],
+zip_safe = False)
+
+
